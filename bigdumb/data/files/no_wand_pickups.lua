44ee31e81e9256d44209a6ac429b8ec45950e35c
tralezab <40974010+tralezab@users.noreply.github.com>
2019-10-26 15:34:56 -0700

    base

---
+++ bigdumb/data/files/no_wand_pickups.lua
@@ -0,0 +1,23 @@
+dofile("data/scripts/lib/utilities.lua")
+
+table.insert( perk_list,
+{
+	id = "NO_WANDS",
+	ui_name = "The Vow",
+	ui_description = "You may not pick up any wands.",
+	ui_icon = "mods/bigdumb/files/thevow.png",
+	perk_icon = "mods/more_perks/files/vowig.png",
+	--not_in_default_perk_pool = cfg_disable_wandbuff,
+	func = function( entity_perk_item, entity_who_picked, item_name )
+		
+		EntityAddComponent( entity_who_picked, "LuaComponent", 
+		{ 
+			script_source_file = "mods/bigdumb/files/kill_you.lua",
+			execute_every_n_frame = "2",
+		} )
+			
+	end,
+		
+})
+
+
---
+++ bigdumb/init.lua
@@ -0,0 +1,43 @@
+dofile( "data/scripts/perks/perk.lua" )
+dofile("mods/bigdumb/data/files/no_wand_pickups.lua")
+
+function OnPlayerSpawned( player_entity ) -- this runs when player entity has been created
+
+	local inventory = nil
+	local x,y = EntityGetTransform( player_entity )
+
+	-- find the quick inventory
+	local player_child_entities = EntityGetAllChildren( player_entity )
+	if ( player_child_entities ~= nil ) then
+		for i,child_entity in ipairs( player_child_entities ) do
+			local child_entity_name = EntityGetName( child_entity )
+			
+			if ( child_entity_name == "inventory_quick" ) then
+				inventory = child_entity
+			end
+		end
+	end
+
+	-- set inventory contents
+	if ( inventory ~= nil ) then
+		local inventory_items = EntityGetAllChildren( inventory )
+		
+		-- remove default items
+		if inventory_items ~= nil then
+			for i,item_entity in ipairs( inventory_items ) do
+				GameKillInventoryItem( player_entity, item_entity )
+			end
+		end
+		-- add taaaablets
+		for i,item_entity in ipairs( {"data/entities/items/books/throwtablet1.xml", "data/entities/items/books/throwtablet2.xml", "data/entities/items/books/throwtablet3.xml", "data/entities/items/books/throwtablet4.xml"} ) do
+			local item_entity = EntityLoad( item_entity )
+			EntityAddChild( inventory, item_entity )
+		end
+	end
+
+	--now the last thing, giving a perk.
+	local perk_entity = perk_spawn( x, y, "NO_WANDS" )
+	if ( perk_entity ~= nil ) then
+		perk_pickup( perk_entity, player_entity, EntityGetName( perk_entity ), false, false )
+	end
+end
